9f34dad6cb5764f5c7e39fcfb22386205173f85a
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-17 15:19:09 +0800

    Update square_pre_B.m

--- DSS/square_pre_B.m
+++ DSS/square_pre_B.m
@@ -5,10 +5,11 @@
 %
 % insert 0s in every 2bit, extend input from 8 to 16 bits
 function out = square_pre_B(a)
-T = 0;
-for i = 1 : 8
-    T = bitset(T, 2*i - 1, bitget(a, i));
-end
-out = T;
-
+    T = 0;
+    for i = 1 : 8
+        T = bitset(T, 2*i - 1, bitget(a, i));
+    end
+    out = T;
 return
+%
+
