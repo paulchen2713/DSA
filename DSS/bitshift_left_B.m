8b45ce0c88eb872ff5e296e00090707157c8ae70
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-06-17 15:11:39 +0800

    Update bitshift_left_B.m

--- DSS/bitshift_left_B.m
+++ DSS/bitshift_left_B.m
@@ -2,15 +2,14 @@
 % bit left-shift by one function
 %
 function out = bitshift_left_B(a)
-global ifx;
-a = uint32(a);
-bit_old = 0; % temp for msb in each row
-for i = 1 : ifx
-    bit = bitget(a(i), 32);
-    a(i) = bitshift(a(i), 1);
-    a(i) = bitset(a(i), 1, bit_old);
-    bit_old = bit;    
-end
-out = double(a);
-
+    global ifx;
+    a = uint32(a), bit_old = 0; % temp for msb in each row
+    for i = 1 : ifx
+        bit = bitget(a(i), 32);
+        a(i) = bitshift(a(i), 1);
+        a(i) = bitset(a(i), 1, bit_old);
+        bit_old = bit;    
+    end
+    out = double(a);
 return
+
