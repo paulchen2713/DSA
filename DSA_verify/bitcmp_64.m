36e3463cb22ce60e7d2e19d6e3fbdade0714ff49
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2022-07-02 08:43:01 +0800

    Update bitcmp_64.m

--- DSA_verify/bitcmp_64.m
+++ DSA_verify/bitcmp_64.m
@@ -11,11 +11,11 @@ function out = bitcmp_64(a)
     a_right_dec = uint32(hex2dec(a_right));
 
     % do bit-complement opertion separately
-    a_left_dec = bitcmp(a_left_dec);
+    a_left_dec  = bitcmp(a_left_dec);
     a_right_dec = bitcmp(a_right_dec);
 
     % turn a back into decimal type
-    a_left_hex = dec2hex(a_left_dec, 8);
+    a_left_hex  = dec2hex(a_left_dec, 8);
     a_right_hex = dec2hex(a_right_dec, 8);
 
     % concatenate left and right string
