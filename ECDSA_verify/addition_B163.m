e64815a25f4151e2c0a5f5af902118f28a03906b
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-10-17 12:30:44 +0800

    Update addition_B163.m

--- ECDSA_verify/addition_B163.m
+++ ECDSA_verify/addition_B163.m
@@ -1,9 +1,10 @@
 %
 % Addition over B163
 %
-function out=addition_B163(ai,bi)
-a=uint32(ai);
-b=uint32(bi);
-c=bitxor(a,b);
-out=double(c);
+function out = addition_B163(ai, bi)
+    a = uint32(ai);
+    b = uint32(bi);
+    c = bitxor(a, b);
+    out = double(c);
 return
+
