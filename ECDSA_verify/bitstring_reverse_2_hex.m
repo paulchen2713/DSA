897b286cb6797f641ed994fe51d523704173a112
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-11-14 09:38:55 +0800

    Update bitstring_reverse_2_hex.m

--- ECDSA_verify/bitstring_reverse_2_hex.m
+++ ECDSA_verify/bitstring_reverse_2_hex.m
@@ -4,11 +4,12 @@
 % input: 0,1 decimal string
 %
 function out=bitstring_reverse_2_hex(bs)
-bs=(flipud(bs'))';
-bs=char(bs+48);
-bs_hex=char();
-for i=0:15
-    bs_hex=strcat(bs_hex,dec2hex(bin2dec(bs(i*4+1:(i+1)*4)),1));
-end
-out=bs_hex;
+    bs=(flipud(bs'))';
+    bs=char(bs+48);
+    bs_hex=char();
+    for i=0:15
+        bs_hex=strcat(bs_hex,dec2hex(bin2dec(bs(i*4+1:(i+1)*4)),1));
+    end
+    out=bs_hex;
 return
+
