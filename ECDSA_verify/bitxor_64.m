89ac891f02cb27b7cc5962214d7fa1890e00714f
Paul <95068443+paulchen2713@users.noreply.github.com>
2022-11-14 09:40:20 +0800

    Update bitxor_64.m

--- ECDSA_verify/bitxor_64.m
+++ ECDSA_verify/bitxor_64.m
@@ -1,22 +1,22 @@
 %
 % bitxor for 64 bits
 %
-function out=bitxor_64(a,b)
-a_left=a(1:8);
-a_right=a(9:16);
-a_left_dec=uint32(hex2dec(a_left));
-a_right_dec=uint32(hex2dec(a_right));
-b_left=b(1:8);
-b_right=b(9:16);
-b_left_dec=uint32(hex2dec(b_left));
-b_right_dec=uint32(hex2dec(b_right));
+function out = bitxor_64(a, b)
+    a_left  = a(1:8);
+    a_right = a(9:16);
+    a_left_dec  = uint32(hex2dec(a_left));
+    a_right_dec = uint32(hex2dec(a_right));
+    
+    b_left  = b(1:8);
+    b_right = b(9:16);
+    b_left_dec  = uint32(hex2dec(b_left));
+    b_right_dec = uint32(hex2dec(b_right));
 
-a_b_left_dec=bitxor(a_left_dec,b_left_dec);
-a_b_right_dec=bitxor(a_right_dec,b_right_dec);
+    a_b_left_dec  = bitxor(a_left_dec, b_left_dec);
+    a_b_right_dec = bitxor(a_right_dec, b_right_dec);
 
-a_b_left=dec2hex(a_b_left_dec,8);
-a_b_right=dec2hex(a_b_right_dec,8);
-
-out=strcat(a_b_left,a_b_right);
+    a_b_left  = dec2hex(a_b_left_dec, 8);
+    a_b_right = dec2hex(a_b_right_dec, 8);
+    out = strcat(a_b_left, a_b_right);
 return
 
