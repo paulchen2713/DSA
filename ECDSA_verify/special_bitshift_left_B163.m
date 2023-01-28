3af753b556a9619876a20e39f8e5d9c62a3d0dca
Paul <95068443+paulchen2713@users.noreply.github.com>
2023-01-28 10:21:06 +0800

    Update special_bitshift_left_B163.m

--- ECDSA_verify/special_bitshift_left_B163.m
+++ ECDSA_verify/special_bitshift_left_B163.m
@@ -1,15 +1,16 @@
 %
 % bit left-shift by one
 %
-function out=special_bitshift_left_B163(ai)
-global ifx;
-a=uint32(ai);
-bit_old=0;
-for i=1:ifx+1
-    bit=bitget(a(i),32);
-    a(i)=bitshift(a(i),1);
-    a(i)=bitset(a(i),1,bit_old);
-    bit_old=bit;    
-end
-out=double(a);
+function out = special_bitshift_left_B163(ai)
+    global ifx;
+    a = uint32(ai);
+    bit_old = 0;
+    for i = 1:ifx+1
+        bit = bitget(a(i), 32);
+        a(i) = bitshift(a(i), 1);
+        a(i) = bitset(a(i), 1, bit_old);
+        bit_old = bit;    
+    end
+    out = double(a);
 return
+
