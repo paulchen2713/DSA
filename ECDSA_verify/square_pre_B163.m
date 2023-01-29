5a31924f506f52e5842299ebd18e93cc00febc63
Paul <95068443+paulchen2713@users.noreply.github.com>
2023-01-29 09:57:25 +0800

    Update square_pre_B163.m

--- ECDSA_verify/square_pre_B163.m
+++ ECDSA_verify/square_pre_B163.m
@@ -1,14 +1,13 @@
 %
 % Mulplication over B-163
-%
-% ref. Guide p.53
+%   ref. Guide p.53
 %
 function out=square_pre_B163(ai)
-a=ai;
-T=0;
-for i=1:8
-    T=bitset(T,2*i-1,bitget(a,i));
-end
-out=T;
+    a=ai;
+    T=0;
+    for i=1:8
+        T=bitset(T,2*i-1,bitget(a,i));
+    end
+    out=T;
 return
 
